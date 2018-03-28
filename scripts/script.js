fd138e8317b1d3d51a18373cb456e49871558300
kari3m <karimtarek340@gmail.com>
2018-03-28 02:31:35 +0200

    s

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 $('button').click( function() {
-                   var counter = $('#cart').val();
-                   counter++ ;
-                   console.log(counter);
-                   $('#cart').val(counter);
-               });
+   var counter = $('#cart').val();
+   counter++ ;
+   console.log(counter);
+  $('#cart').val(counter);
+});
 var cart = [];
 $('button').click(function(){
   cart = $('#cart');
-  cart.push($(.selectedImg));
+  cart.push($('.selectedImg'));
 
 });
